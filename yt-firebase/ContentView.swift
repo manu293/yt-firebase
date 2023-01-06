85830e5cf1d1eea8fb9c01ab8a2704d78a74d54b
Manoj S M <manojsm293@gmail.com>
2023-01-06 12:57:46 +0530

    feat: Base Project Setup
    Actions taken:
    - Initailise a new swift ui project

---
+++ yt-firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yt-firebase
+//
+//  Created by Propertyloop02 on 06/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yt-firebase/yt_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  yt_firebaseApp.swift
+//  yt-firebase
+//
+//  Created by Propertyloop02 on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct yt_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
